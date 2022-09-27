6a6d4b5aba4f218550d8ff83e341d8436e46eec9
Marc-Eid <marc.mn.eid@gmail.com>
2022-09-27 10:57:27 -0400

    This Event (MOVED_TO) happens once. so will be listening to it.

--- app/src/main/java/com/example/imageduplicator/DirectoryObserver.java
+++ app/src/main/java/com/example/imageduplicator/DirectoryObserver.java
@@ -10,20 +10,22 @@ import java.util.stream.Stream;
 
 public class DirectoryObserver extends FileObserver {
 
-    String aboslutePath = "path to your directory";
+    String aboslutePath;
 
 
     @SuppressLint("NewApi")
     public DirectoryObserver(File file) {
-        super(file);
+        super(file, FileObserver.MOVED_TO);
         aboslutePath = file.getPath();
     }
 
     @SuppressLint("NewApi")
     @Override public void onEvent(int event, String path) {
-        Log.e("FileObserver" , event + "");
+
         if(path != null)
         {
+
+            Log.e("FileObserver" , event + "");
             Log.e("FileObserver: ","File Created");
 
             //to-do: capture last file along some files within +/- 30s
